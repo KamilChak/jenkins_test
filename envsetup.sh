096d0da426ae016bed88c69faa75a9d74947bb06
KamilChak <odragon707@gmail.com>
2024-08-15 21:22:42 +0100

    .

--- envsetup.sh
+++ envsetup.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# Check if the virtual environment directory exists
 if [ ! -d "env" ]; then
     echo "Creating Python virtual environment..."
     python3 -m venv env
 fi
 
-source env/bin/activate
+# Activate the virtual environment
+. env/bin/activate
 
+# Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
 
+# Create logs directory if it doesn't exist
 if [ ! -d "logs" ]; then
     echo "Creating logs directory..."
     mkdir logs
     touch logs/error.log logs/access.log
 fi
 
+# Set permissions for logs
 chmod -R 777 logs
 
 echo "Environment setup finished."
